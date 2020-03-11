47b89055479aa69e8e856e93f232df7bf9904ee6
Cola Cheng <imlikedog@gmail.com>
2020-03-11 17:33:10 +0800

    Remove url hint.

--- lib/ex_trends/request.ex
+++ lib/ex_trends/request.ex
@@ -1,15 +1,4 @@
 defmodule ExTrends.Request do
-  # python ref: https://github.com/GeneralMills/pytrends
-  # @general_url "https://trends.google.com/trends/api/explore"
-  # @interest_over_time_url "https://trends.google.com/trends/api/widgetdata/multiline"
-  # @interest_by_region_url "https://trends.google.com/trends/api/widgetdata/comparedgeo"
-  # @related_queries_url "https://trends.google.com/trends/api/widgetdata/relatedsearches"
-  # @trending_searches_url "https://trends.google.com/trends/hottrends/visualize/internal/data"
-  # @top_charts_url "https://trends.google.com/trends/api/topcharts"
-  # @suggestions_url "https://trends.google.com/trends/api/autocomplete/"
-  # @categories_url "https://trends.google.com/trends/api/explore/pickers/category"
-  # @daily_trends_url "https://trends.google.com/trends/api/dailytrends"
-
   @default_opts [recv_timeout: 30_000]
 
   def request(method, url, body \\ "", headers \\ [], http_opts \\ []) do
