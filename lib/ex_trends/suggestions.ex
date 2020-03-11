003ef26f749cba9f7b7a6d86be88c312adc8c04d
Cola Cheng <imlikedog@gmail.com>
2020-03-11 15:57:12 +0800

    Add suggestions operation.

---
+++ lib/ex_trends/suggestions.ex
@@ -0,0 +1,43 @@
+defmodule ExTrends.Suggestions do
+  defstruct keyword: nil,
+            time: "today 5-y",
+            geo: "US",
+            hl: "en-US",
+            tz: "360",
+            prop: "",
+            cat: 0
+
+  @spec request(%{
+          required(:keyword) => binary,
+          optional(:time) => binary,
+          optional(:geo) => binary,
+          optional(:hl) => binary,
+          optional(:tz) => binary,
+          optional(:prop) => binary,
+          optional(:cat) => integer
+        }) :: ExTrends.Operation.Suggestions.t()
+  def request(%{keyword: keyword} = query) do
+    %{hl: hl, tz: tz} =
+      explore_query =
+      %ExTrends.Suggestions{}
+      |> struct(query)
+      |> Map.from_struct()
+      |> Map.put(:keywords, [keyword])
+
+    case ExTrends.Explore.request(explore_query) |> ExTrends.request() do
+      {:ok, [%{"request" => request, "token" => token} | _]} ->
+        %{path: path} = suggestions = %ExTrends.Operation.Suggestions{}
+        req = :jiffy.encode(request)
+
+        %ExTrends.Operation.Suggestions{
+          suggestions
+          | params: [hl: hl, tz: tz, req: req, token: token],
+            path: path <> "/" <> URI.encode(keyword)
+        }
+        |> struct()
+
+      _ ->
+        {:error, :error}
+    end
+  end
+end
